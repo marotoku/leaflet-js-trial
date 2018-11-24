4efd15cd1fbdb196f49f94b2bacf46153d59e22d
marotoku@gmail.com <marotoku@gmail.com>
2018-11-24 16:02:11 +0900

    北海道の境界データを読み込むように変更

--- js/trial.js
+++ js/trial.js
@@ -7,35 +7,19 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 mymap.setView([43.0620306, 141.3543755], 7);
 
+function getJSON(jsonFile) {
+  var req = new XMLHttpRequest();                 // XMLHttpRequest オブジェクトを生成する
+  req.onreadystatechange = function() {           // XMLHttpRequest オブジェクトの状態が変化した際に呼び出されるイベントハンドラ
+    if(req.readyState == 4 && req.status == 200){ // サーバーからのレスポンスが完了し、かつ、通信が正常に終了した場合
+      return JSON.parse(req.responseText);    // 取得した JSON ファイルの中身を変数へ格納
+    }
+  };
+  req.open("GET", jsonFile, false);          // HTTPメソッドとアクセスするサーバーのURLを指定
+  req.send(null);                            // 実際にサーバーへリクエストを送信
+}
 
-var markerIcon = L.icon({
-  iconUrl: 'http://www.nowhere.co.jp/blog/wp-content/uploads/2018/07/marker.png', // アイコン画像のURL
+var hokkaido = getJson("../data/01.json");
 
-  iconSize: [32, 32], // アイコンの大きさ
-  iconAnchor: [16, 32], // 画像内でマーカーの位置を指し示す点の位置
-  popupAnchor: [0, -32] // ポップアップが出現する位置（iconAnchorからの相対位置）
-});
-
-var marker01 = L.marker([37.508106, 139.930239], {
-  icon: markerIcon
-}).addTo(mymap);
-var marker02 = L.marker([37.494833, 139.929750], {
-  icon: markerIcon
-}).addTo(mymap);
-
-marker01.bindPopup("会津若松駅");
-marker02.bindPopup("会津若松市役所");
-
-var route = L.polyline([
-  [37.507543, 139.930619],
-  [37.507016, 139.932408],
-  [37.498371, 139.928649],
-  [37.495337, 139.928006],
-  [37.495123, 139.929800],
-  [37.494833, 139.929750]
-], {
-  color: "blue",
-  weight: 6
-}).addTo(mymap);
-
-route.bindPopup("駅から市役所までの経路");
+var boundary = L.geoJson(hokkaido, {
+        style: style,
+}).addTo(map);
